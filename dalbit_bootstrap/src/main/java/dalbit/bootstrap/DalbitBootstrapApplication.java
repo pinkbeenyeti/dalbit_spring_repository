b988f8e1a746bbb56383403d08a75484c92f5929
pinkbeenyeti <rbgusdn12@gmail.com>
2026-04-03 00:31:39 +0900

    feat(bootstrap): 애플리케이션 진입점 및 환경 변수 기반 설정 완료

---
+++ dalbit_bootstrap/src/main/java/dalbit/bootstrap/DalbitBootstrapApplication.java
@@ -0,0 +1,15 @@
+package dalbit.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication(scanBasePackages = "dalbit")
+@ConfigurationPropertiesScan(basePackages = "dalbit")
+public class DalbitBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DalbitBootstrapApplication.class, args);
+    }
+
+}
