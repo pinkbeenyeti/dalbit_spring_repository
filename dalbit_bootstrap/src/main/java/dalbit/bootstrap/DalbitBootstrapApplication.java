f88e8f733b6a7e6ee15a39f5238655522b23ad2c
pinkbeenyeti <rbgusdn12@gmail.com>
2026-04-06 12:04:30 +0900

    fix: 시큐리티의 '기본 유저 생성' 자동 설정을 제외

--- dalbit_bootstrap/src/main/java/dalbit/bootstrap/DalbitBootstrapApplication.java
+++ dalbit_bootstrap/src/main/java/dalbit/bootstrap/DalbitBootstrapApplication.java
@@ -2,9 +2,10 @@ package dalbit.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = "dalbit")
+@SpringBootApplication(scanBasePackages = "dalbit", exclude = { UserDetailsServiceAutoConfiguration.class })
 @ConfigurationPropertiesScan(basePackages = "dalbit")
 public class DalbitBootstrapApplication {
 
