7ba168e6b42939df0c69426bcd62930d70af3930
pinkbeenyeti <rbgusdn12@gmail.com>
2026-04-16 22:17:05 +0900

    feat: enforce audio/wav content-type for R2 presigned upload URLs

--- dalbit_adapter_storage/src/main/java/dalbit/adapter/storage/r2/out/R2Adapter.java
+++ dalbit_adapter_storage/src/main/java/dalbit/adapter/storage/r2/out/R2Adapter.java
@@ -83,6 +83,7 @@ public class R2Adapter implements GenerateUploadUrlPort, VerifyUploadPort {
             PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(r2Properties.bucketName())
                 .key(path)
+                .contentType("audio/wav")
                 .build();
 
             PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
